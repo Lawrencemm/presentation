663b4a2efc05b88342f5b1cd791c00d07e6fed78
Lawrence Millar-Madigan <lawrencem99@gmail.com>
2019-03-02 16:50:06 +1100

    Create example CMake producer library.

---
+++ library.cpp
@@ -0,0 +1,11 @@
+#include "library.h"
+
+#include <cstdlib>
+
+#include <iostream>
+
+void speak() {
+    std::cout << "Python sucks!\n";
+    std::cout << "Have a random number for your troubles!\n" ;
+    std::cout << rand() << std::endl;
+}
